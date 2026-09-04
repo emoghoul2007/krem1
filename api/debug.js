eeb1816f902edb9a1320d8acf6a859d001e38a17
emoghoul2007 <apfs06990@gmail.com>
2026-09-04 19:50:53 +0300

    debug.js

--- api/debug.js
+++ api/debug.js
@@ -1,1 +1,1 @@
-export default function handler(req, res) { const key = process.env.LP_CRM_API_KEY; res.status(200).json({ hasKey: !!key, keyLength: key ? key.length : 0, keyStart: key ? key.slice(0, 6) : null }); }
+keyEnd: key ? JSON.stringify(key.slice(-8)) : null
